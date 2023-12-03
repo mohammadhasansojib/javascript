fc7dcd1a36665bce7da1b6a17dd308804b4a5825
sajib hasan <sajibhasan800080@gmail.com>
2023-12-04 00:03:49 +0600

    added code in index.html & script.js of 'data-types' folder

---
+++ data-types/script.js
@@ -0,0 +1,16 @@
+var a = "hello world";
+a = 50;
+a = true;
+a = null;
+a = ["html","css","js"];
+a = {
+    name: "sajib",
+    age: 40
+};
+
+
+
+
+document.write("<h1>",a,"</h1>");
+
+document.write("<h2>",typeof a,"</h2>");
