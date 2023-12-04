10a8b95b53919b02207414a52cdb2b3db5845a01
sajib hasan <sajibhasan800080@gmail.com>
2023-12-04 23:08:41 +0600

    added code in index.html & script.js of 'if-else-statement-2' folder

---
+++ if-else-statement-2/script.js
@@ -0,0 +1,10 @@
+console.clear();
+
+let name = "sajib hasan";
+let gender = "male";
+
+if(gender == "male"){
+    console.log("hello mr. " + name);
+}else{
+    console.log("hello miss. " + name);
+}
