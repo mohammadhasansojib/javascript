cf88776e477bf8d1a05cbda308c0e1762f9a2bc6
sajib hasan <sajibhasan800080@gmail.com>
2023-12-04 22:08:59 +0600

    added code in index.html & script.js of 'comparison-operator' folder

---
+++ comparison-operator/script.js
@@ -0,0 +1,19 @@
+console.clear();
+
+let a = 30 == "30";
+let b = 30 === "30";
+let c = 40 != "40";
+let d = 40 !== "40";
+let e = 40 < "50";
+let f = 40 > "50";
+let g = 40 <= "50";
+let h = 40 >= "50";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
