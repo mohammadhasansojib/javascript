d616881929e82c1ce00c8befe966bf895149cb97
sajib hasan <sajibhasan800080@gmail.com>
2023-12-04 19:39:33 +0600

    added structure & code in index.htm & script.js of 'browser-console' folder

---
+++ browser-console/script.js
@@ -0,0 +1,14 @@
+console.clear();
+
+
+
+
+console.time();
+console.table([50,20,"hello"]);
+
+console.error("there is an issue detected.");
+console.warn("there is an warning.");
+console.timeEnd();
+
+console.log(document.getElementById("main").innerHTML);
+console.log(document.getElementById("main").innerText);
