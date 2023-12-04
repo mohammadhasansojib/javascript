cd0d926c08d54e43074d70aa3496298778f268ea
sajib hasan <sajibhasan800080@gmail.com>
2023-12-04 22:21:48 +0600

    added code in index.html & script.js of 'if-else-statement' folder

---
+++ if-else-statement/script.js
@@ -0,0 +1,18 @@
+console.clear();
+
+let a = 50;
+let b = 30;
+
+
+if(null == undefined){
+    console.log("null and undefined is same in term's of value.")
+}
+
+if(null === undefined){
+    console.log("null and undefined is same in term's of value & data type");
+}
+
+if(a > b){
+    console.log("a is greater than b");
+}
+
