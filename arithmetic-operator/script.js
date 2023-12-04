66ea3ca7e4a5cb7cfe0be1b8be4dfe9251bf7ae8
sajib hasan <sajibhasan800080@gmail.com>
2023-12-04 14:25:50 +0600

    added structure & code in index.html & script.js of 'arithmetic-operator' folder

---
+++ arithmetic-operator/script.js
@@ -0,0 +1,15 @@
+// addition -> +
+// substraction -> -
+// multiplication -> *
+// division -> /
+// exponentiation -> **
+// modulus -> %
+// increment -> ++
+// decrement -> --
+
+var a = 20;
+let b = 6;
+
+// a++;
+// let c = a-- + 1;
+document.write(a++);
