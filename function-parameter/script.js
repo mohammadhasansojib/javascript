27353771e5b023306d358b50663e478b8de698d1
sajib hasan <sajibhasan800080@gmail.com>
2023-12-06 16:08:37 +0600

    added structure & code in index.html & script.js of 'function-parameter' folder

---
+++ function-parameter/script.js
@@ -0,0 +1,19 @@
+console.clear();
+
+
+function addition(a,b){
+    console.log(a+b);
+}
+
+let substraction = (a = 0, b = 0) => {
+    console.log(a-b);
+}
+
+addition(4,6);
+
+addition(30,14);
+
+addition();
+
+substraction(40,20);
+substraction();
