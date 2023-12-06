c3d47a9b9c69ca30edb1b4b960ef7f42402b6162
sajib hasan <sajibhasan800080@gmail.com>
2023-12-07 00:17:07 +0600

    added structure & code in index.html & script.js of 'while-loop' folder

---
+++ while-loop/script.js
@@ -0,0 +1,19 @@
+let a = 1;
+
+
+document.write("<ol>");
+
+while(a <= 10){
+    document.write("<h2><li>"+"HELLO WORLD"+"</li></h2>");
+    a++;
+}
+
+document.write("</ol>");
+
+
+document.write("<br>");
+let b = 10;
+while(b >= 1){
+    document.write("<h2>"+b+"</h2>");
+    b--;
+}
