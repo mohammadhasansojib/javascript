cbe2a887e7f7d95d97236644b01f41c81d9c9709
sajib hasan <sajibhasan800080@gmail.com>
2023-12-07 11:20:44 +0600

    added code in index.html & script.js of 'even-odd-numbers' folder

---
+++ even-odd-numbers/script.js
@@ -0,0 +1,17 @@
+console.clear();
+
+
+for(let i = 1; i <= 30; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
+for(let i = 0; i < 2; i++){
+    console.log(" ");
+}
+
+
+for(let i = 1; i <= 30; i += 2){
+    console.log(i);
+}
