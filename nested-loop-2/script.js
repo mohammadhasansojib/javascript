07a10914f5ad640dcaf5a823f90d51c82434e021
sajib hasan <sajibhasan800080@gmail.com>
2023-12-07 16:35:46 +0600

    added code in index.html & script.js of 'nested-loop-2' folder

---
+++ nested-loop-2/script.js
@@ -0,0 +1,46 @@
+
+
+
+
+for(let i = 1; i <= 5; i++){
+    for(let j = 1; j <= i; j++){
+        document.write(j+" ");
+    }
+    document.write("<br>");
+}
+
+document.write("<br>");
+
+for(let i = 5; i > 0; i--){
+    for(let j = 1; j <= i; j++){
+        document.write(j+" ");
+    }
+    document.write("<br>");
+}
+
+document.write("<br>");
+
+for(let i = 5; i > 0; i--){
+    for(let j = i; j > 0; j--){
+        document.write(i+" ");
+    }
+    document.write("<br>");
+}
+
+document.write("<br>");
+
+for(let i = 5; i > 0; i--){
+    for(let j = i; j > 0; j--){
+        document.write(j+" ");
+    }
+    document.write("<br>");
+}
+
+document.write("<br>");
+
+for(let i = 5; i >= 1; i--){
+    for(let j = 1; j <= i; j++){
+        document.write(j+" ");
+    }
+    document.write("<br>");
+}
