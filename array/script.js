b1cfd0206d7f15b85d2fbbac972a75c7c882c8e9
sajib hasan <sajibhasan800080@gmail.com>
2023-12-07 18:19:07 +0600

    added code in index.html & script.js of 'array' folder

---
+++ array/script.js
@@ -0,0 +1,20 @@
+console.clear();
+
+
+
+let arr = [10,20,30,40,50];
+let sum = 0;
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+
+    sum = sum + arr[i];
+}
+
+console.log(sum);
+
+
+
+let arr2 = [30,"hello",true,null,undefined];
+
+console.log(arr2[3]);
