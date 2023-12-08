5775610343b8da6b7c7062a2a48da211c94290dd
sajib hasan <sajibhasan800080@gmail.com>
2023-12-08 20:10:32 +0600

    added code in index.html script.js of 'delete-modify-array-elements' folder

---
+++ delete-modify-array-elements/script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+let arr = ["hello",30,"Bsc"];
+
+document.write(arr);
+
+arr[0] = "world";
+
+document.write("<br>",arr);
+
+delete arr[1];
+
+document.write("<br>",arr);
+
+document.write("<br>", arr[1]);
