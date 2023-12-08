d68c3657d7ecde8da0701cab996c5d9970265512
sajib hasan <sajibhasan800080@gmail.com>
2023-12-08 20:24:53 +0600

    added code in index.html & script.js of 'sort-reverse-method-array' folder

---
+++ sort-reverse-method-array/script.js
@@ -0,0 +1,43 @@
+
+
+
+
+let arr = ["sajid","rajon","jisan","nisan"];
+
+document.write(arr,"<br>");
+
+arr.sort();
+
+document.write(arr,"<br>");
+
+arr.reverse();
+
+document.write(arr,"<br>");
+
+
+
+
+let arr2 = [40,10,60,30];
+
+document.write(arr2,"<br>");
+
+arr2.sort();
+
+document.write(arr2,"<br>");
+
+arr2.reverse();
+
+document.write(arr2,"<br>");
+
+let arr3 = ["hello","alif",30,20];
+
+document.write(arr3,"<br>");
+
+arr3.sort();
+
+document.write(arr3,"<br>");
+
+arr3.reverse();
+
+document.write(arr3,"<br>");
+
