45802820869f64fa6f09b4425c7489c7ded5d90c
sajib hasan <sajibhasan800080@gmail.com>
2023-12-09 00:18:03 +0600

    added code in index.html & script.js of 'concat-join-method-array' folder

---
+++ concat-join-method-array/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+let a = ["hasan","rubel","nijam"];
+let b = ["rohan","jiban"];
+let c = ["bujo na"];
+
+let result = a.concat(b,"boro","choto",c);
+
+document.write(result,"<br><br>");
+
+
+let result2 = a.join(" -$- ");
+
+document.write(result2,"<br><br>");
