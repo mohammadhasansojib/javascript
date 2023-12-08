bac94ea07529cc9a369f62abbdae10a9538b63f4
sajib hasan <sajibhasan800080@gmail.com>
2023-12-08 23:41:25 +0600

    added code in index.html & script.js of 'pop-push-method-array' folder

---
+++ pop-push-method-array/script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+let arr = ["sadman","rajon","naim","meraj"];
+
+document.write(arr+"<br><br>");
+
+arr.pop();
+
+document.write(arr+"<br><br>");
+
+arr.push(50);
+
+document.write(arr+"<br><br>");
+
+arr.push("rafat");
+
+document.write(arr+"<br><br>");
