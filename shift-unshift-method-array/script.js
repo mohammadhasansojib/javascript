57e79c579c2bfcd69501836fd0b9b6baec8b363f
sajib hasan <sajibhasan800080@gmail.com>
2023-12-08 23:57:47 +0600

    added code in index.html & script.js of 'shift-unshift-method-array' folder

---
+++ shift-unshift-method-array/script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+let arr = ["rakib","jenom","nishan","mamun"];
+
+document.write(arr,"<br><br>");
+
+arr.shift();
+
+document.write(arr,"<br><br>");
+document.write(arr[0],"<br><br>");
+
+arr.unshift("hello");
+
+document.write(arr,"<br><br>");
+
+arr.unshift("brown");
+
+document.write(arr,"<br><br>");
