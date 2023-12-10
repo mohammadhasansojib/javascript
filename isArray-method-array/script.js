4d780b5ba0d2b343f5e6bfc93fba0dac0cb4a8b2
sajib hasan <sajibhasan800080@gmail.com>
2023-12-10 22:58:35 +0600

    added code in index.html & script.js of 'isArray-method-array' folder

---
+++ isArray-method-array/script.js
@@ -0,0 +1,17 @@
+
+
+
+let arr = [20,50,20,48];
+
+let a = 20;
+let b = "hello";
+
+let result = array.isArray(arr);
+
+document.write(result);
+
+if(result){
+    document.write("<br><br>This is an array");
+}else{
+    document.write("<br><br>This is not an array");
+}
