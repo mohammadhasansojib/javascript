6faba534248267525edb22571da59fa8ec86dc4d
sajib hasan <sajibhasan800080@gmail.com>
2023-12-10 23:23:26 +0600

    added code index.html & script.js of 'indexOf-lastIndexOf-method-array' folder

---
+++ indexOf-lastIndexOf-method-array/script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+let arr = ["hello","sigma","chad","hello","joss","opps"];
+
+
+let a = arr.indexOf("hello",1);
+let b = arr.indexOf("hello");
+let c = arr.lastIndexOf("hello");
+let d = arr.lastIndexOf("hello", 2);
+let e = arr.indexOf("wow");
+
+document.write(a+"<br><br>");
+document.write(b+"<br><br>");
+document.write(c+"<br><br>");
+document.write(d+"<br><br>");
+document.write(e+"<br><br>");
