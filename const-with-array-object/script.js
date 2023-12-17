54c1c798b3cf3ec1803966d20ed448d5d4fd530f
sajib hasan <sajibhasan800080@gmail.com>
2023-12-17 23:30:42 +0600

    added code in index.html & script.js of 'const-with-array-object' folder

---
+++ const-with-array-object/script.js
@@ -0,0 +1,28 @@
+
+
+
+
+const obj1 = {
+    fname : "sajib",
+    lname : "hasan",
+    age : 20
+};
+
+const obj2 = new Object();
+
+
+document.write(obj1.fname);
+document.write("<br><br>");
+
+obj1.fname = "rhan";
+
+
+document.write(obj1.fname);
+document.write("<br><br>");
+
+obj2.fname = "jisan";
+
+document.write(obj2.fname);
+document.write("<br><br>");
+
+
