9a0bc050b72eee0ca2a961a12deff6dbdc18d541
sajib hasan <sajibhasan800080@gmail.com>
2023-12-19 00:30:49 +0600

    added code in index.html & script.js of 'string-method-2' folder

---
+++ string-method-2/script.js
@@ -0,0 +1,45 @@
+
+
+
+
+let str = "Hello World";
+let str2 = "How are you    r";
+let str3 = "OK";
+let num = 40;
+
+let a = str.charAt(6);
+let b = str.charCodeAt("6");
+let c = String.fromCharCode(97);
+let d = str.concat(" ",str2," ",str3);
+let e = str2.trim().split(" ");
+let f = str.repeat(4);
+let g = str.slice(1,7);
+let h = str.substring(1,7);
+let i = str.substr(1,8);
+let j = num.toString();
+let k = str.valueOf();
+
+
+
+
+document.write(a);
+document.write("<br><br>");
+document.write(b);
+document.write("<br><br>");
+document.write(c);
+document.write("<br><br>");
+document.write(d);
+document.write("<br><br>");
+document.write(e);
+document.write("<br><br>");
+document.write(f);
+document.write("<br><br>");
+document.write(g);
+document.write("<br><br>");
+document.write(h);
+document.write("<br><br>");
+document.write(i);
+document.write("<br><br>");
+document.write(j+40);
+document.write("<br><br>");
+document.write(k);
