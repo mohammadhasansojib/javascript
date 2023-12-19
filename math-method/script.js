e9374966075bdf1e4299ccd94d4fa6cc5efb3046
sajib hasan <sajibhasan800080@gmail.com>
2023-12-19 12:04:08 +0600

    added code in index.html & script.js of 'math-method' folder

---
+++ math-method/script.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+let a = Math.ceil(43.3);
+let b = Math.floor(43.9);
+let c = Math.trunc(43.9);
+let d = Math.round(43.9);
+let e = Math.max(4,4.3,8.2,6);
+let f = Math.min(9,4,2,6);
+let g = Math.sqrt(64);
+let h = Math.cbrt(216);
+let i = Math.pow(4,2);
+let j = Math.round(Math.random() * 10);
+let k = Math.PI.toFixed(4);
+let l = Math.abs(-4.83);
+
+
+
+document.write(a);
+document.write("<br><br>");
+document.write(b);
+document.write("<br><br>");
+document.write(c);
+document.write("<br><br>");
+document.write(d);
+document.write("<br><br>");
+document.write(e);
+document.write("<br><br>");
+document.write(f);
+document.write("<br><br>");
+document.write(g);
+document.write("<br><br>");
+document.write(h);
+document.write("<br><br>");
+document.write(i);
+document.write("<br><br>");
+document.write(j);
+document.write("<br><br>");
+document.write(k);
+document.write("<br><br>");
+document.write(l);
+document.write("<br><br>");
