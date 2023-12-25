6813599aa72dbe42650cbe76ff32ae8e3daff305
sajib hasan <sajibhasan800080@gmail.com>
2023-12-25 16:36:16 +0600

    added code in index.html & script.js of 'dom-targeting-method' folder

---
+++ dom-targeting-method/script.js
@@ -0,0 +1,42 @@
+console.clear();
+
+
+
+
+let a = document;
+let b = document.all;
+let c = document.all[6];
+let d = document.title;
+let e = document.links;
+
+let f = document.images;
+let g = document.baseURI;
+let h = document.domain;
+let i = document.documentElement;
+let j = document.URL;
+let k = document.head;
+let l = document.anchors;
+
+let m = document.getElementById("blust");
+let n = document.getElementsByClassName("hello");
+let o = document.getElementsByClassName("hello")[0];
+let p = document.getElementsByTagName("a");
+let q = document.getElementsByTagName("a")[1];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+console.log(n);
+console.log(o);
+console.log(p);
+console.log(q);
