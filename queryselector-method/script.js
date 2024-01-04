8a30e299994f4e1d1094a148149c12df8febfecf
sajib hasan <sajibhasan800080@gmail.com>
2024-01-04 21:14:14 +0600

    added code in index.html & script.js of 'queryselector-method' folder

---
+++ queryselector-method/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+let a = document.querySelector(".abc").innerHTML;
+let b = document.querySelector("#hello");
+let c = document.querySelector("ul li");
+
+let d = document.querySelectorAll("ul li")[2];
+let e = document.querySelectorAll(".abc");
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
