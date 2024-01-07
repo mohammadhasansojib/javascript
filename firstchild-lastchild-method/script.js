95e6e4d055a37006ed85c80e31fea4b8c3937e23
sajib hasan <sajibhasan800080@gmail.com>
2024-01-07 23:34:03 +0600

    added code in index.html & script.js of 'firstchild-lastchild-method' folder

---
+++ firstchild-lastchild-method/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+let d = document.querySelector(".inner").firstChild;
+let e = document.querySelector(".inner").firstElementChild;
+console.log(d);
+console.log(e);
+
+
+
+let f = document.querySelector(".inner").lastChild;
+let g = document.querySelector(".inner").lastElementChild;
+console.log(f);
+console.log(g);
