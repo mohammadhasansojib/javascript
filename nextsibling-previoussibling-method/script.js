651661ddab6c6ac9413a073e793d6df9bbfcdf18
sajib hasan <sajibhasan800080@gmail.com>
2024-01-07 23:38:17 +0600

    added code in index.html & script.js of 'nextsibling-previoussibling-method' folder

---
+++ nextsibling-previoussibling-method/script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+let h = document.querySelector(".c").nextSibling;
+let i = document.querySelector(".c").nextElementSibling;
+console.log(h);
+console.log(i);
+
+
+
+let j = document.querySelector(".c").previousSibling;
+let k = document.querySelector(".c").previousElementSibling;
+console.log(j);
+console.log(k);
