f745c6e7b19648c29835ffd3f2a455469961a030
sajib hasan <sajibhasan800080@gmail.com>
2024-01-07 23:30:20 +0600

    added code in index.html & script.js of 'children-childrennodes-method' folder

---
+++ children-childrennodes-method/script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+let a = document.querySelector(".inner").children;
+let b = document.querySelector(".inner").children[0];
+let c = document.querySelector(".inner").childNodes;
+
+console.log(a);
+console.log(b);
+console.log(c);
