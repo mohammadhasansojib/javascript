56d89496229fdb012ed0c68f7c8eff8377cc6c9a
sajib hasan <sajibhasan800080@gmail.com>
2024-01-07 22:31:32 +0600

    added code in index.html & script.js of 'parentelement-parentnode-method' folder

---
+++ parentelement-parentnode-method/script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+let a = document.querySelector(".inner").parentElement;
+let b = document.querySelector("html").parentElement;
+let c = document.querySelector("html").parentNode;
+
+console.log(a);
+console.log(b);
+console.log(c);
